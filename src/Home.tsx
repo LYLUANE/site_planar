1e450e282a9630fdb9fe5713278f4e16cd310332
DEVelyn <121120198+LYLUANE@users.noreply.github.com>
2026-09-22 12:43:12 -0300

    carrinho adicionado, porem, não funcional

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import Head from "./Head";
 import Major from "./Major";
 import Signboard from "./Signboard";
 
+
+export interface Produto {
+  id: number;
+  nome: string;
+  legenda: string;
+}
+
 function Home() {
+
+
+  const [carrinho, setCarrinho] = useState<Produto[]>([]);
+
+  const adicionarAoCarrinho = (produto: Produto) => {
+    setCarrinho((prev) => [...prev, produto]);
+  };
   return (
     <>
-      <Head />
-      <Major />
+      <Head carrinho={carrinho}/>
+      <Major AddCarrinho={adicionarAoCarrinho}/>
       <Signboard />
     </>
   );
